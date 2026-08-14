743aefb0dc753cfeb6b610e810f22d71e1fd5cce
G33l0 <121323412+G33l0@users.noreply.github.com>
2026-08-14 07:36:04 -0700

    Update __init__.py

--- app/dol/schedules/__init__.py
+++ app/dol/schedules/__init__.py
@@ -1,15 +1,9 @@
-"""
-Form 5500 schedule processing.
-"""
-
 from app.dol.schedules.base import ScheduleDefinition
-from app.dol.schedules.default_registry import (
-    build_default_registry,
-)
+from app.dol.schedules.default_registry import build_default_registry
 from app.dol.schedules.registry import ScheduleRegistry
 
-__all__ = [
+__all__ = (
     "ScheduleDefinition",
     "ScheduleRegistry",
     "build_default_registry",
-]
+)
